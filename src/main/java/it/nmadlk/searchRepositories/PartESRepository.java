f97bea086acfc5009be84e995a3aa1555cee98ea
nmadlk <nirmalauwucst@gmail.com>
2019-02-15 19:58:14 +0530

    Configured spring data elasticsearch with spring boot 2.1.0, elasticsearch 6.4.2

--- src/main/java/it/nmadlk/searchRepositories/PartESRepository.java
+++ src/main/java/it/nmadlk/searchRepositories/PartESRepository.java
@@ -1,7 +1,12 @@
-package it.nmadlk.serachRepositories;
+package it.nmadlk.searchRepositories;
 
 import it.nmadlk.models.Part;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+
+/**
+ * @author Navo
+ *
+ */
 public interface PartESRepository extends ElasticsearchRepository<Part, String> {
 }
--- src/main/java/it/nmadlk/searchRepositories/VehicleESRepository.java
+++ src/main/java/it/nmadlk/searchRepositories/VehicleESRepository.java
@@ -1,7 +1,11 @@
-package it.nmadlk.serachRepositories;
+package it.nmadlk.searchRepositories;
 
 import it.nmadlk.models.Vehicle;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+/**
+ * @author Navo
+ *
+ */
 public interface VehicleESRepository extends ElasticsearchRepository<Vehicle, String> {
 }
