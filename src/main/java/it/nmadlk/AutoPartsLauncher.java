f1f44a8a3401cb355c7662ed1f696145ae909271
nmadlk <nirmalauwucst@gmail.com>
2019-02-15 19:57:15 +0530

    Configured spring data elasticsearch with spring boot 2.1.0, elasticsearch 6.4.2

--- src/main/java/it/nmadlk/AutoPartsLauncher.java
+++ src/main/java/it/nmadlk/AutoPartsLauncher.java
@@ -5,6 +5,7 @@ package it.nmadlk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * @author Navo
--- src/main/java/it/nmadlk/configuration/DbRepositoryConfiguration.java
+++ src/main/java/it/nmadlk/configuration/DbRepositoryConfiguration.java
@@ -1,4 +1,46 @@
 package it.nmadlk.configuration;
 
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.net.InetAddress;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "it.nmadlk.repositories")
+@EnableElasticsearchRepositories(basePackages = "it.nmadlk.searchRepositories")
 public class DbRepositoryConfiguration {
+
+    @Value("${elasticsearch.host}")
+    private String elasticsearchHost;
+
+    @Value("${elasticsearch.port}")
+    private int elasticsearchPort;
+
+    @Value("${elasticsearch.cluster.name}")
+    private String elasticsearchClusterName;
+
+    @Bean
+    public Client client() throws Exception {
+
+        Settings esSettings = Settings.builder()
+                .put("cluster.name", elasticsearchClusterName)
+                .build();
+
+        //https://www.elastic.co/guide/en/elasticsearch/guide/current/_transport_client_versus_node_client.html
+        return new PreBuiltTransportClient(esSettings)
+                .addTransportAddress(
+                        new TransportAddress(InetAddress.getByName(elasticsearchHost), elasticsearchPort));
+    }
+
+   /* @Bean
+    public ElasticsearchOperations elasticsearchTemplate() throws Exception {
+        return new ElasticsearchTemplate(client());
+    }*/
 }
