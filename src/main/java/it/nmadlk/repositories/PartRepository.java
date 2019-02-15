41f604ccd6a92b7bf75dfc2d0190450c8a6c03a4
nmadlk <nirmalauwucst@gmail.com>
2019-02-15 19:56:19 +0530

    Configured spring data elasticsearch with spring boot 2.1.0, elasticsearch 6.4.2

--- src/main/java/it/nmadlk/repositories/PartRepository.java
+++ src/main/java/it/nmadlk/repositories/PartRepository.java
@@ -1,10 +1,11 @@
 /**
  * 
  */
-package it.nmadlk.repository;
+package it.nmadlk.repositories;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import it.nmadlk.models.Part;
@@ -13,12 +14,12 @@ import it.nmadlk.models.Part;
  * @author Navo
  * This is an out-of the box solution of Spring Project for building standard CRUD operations/methods
  * Spring Data JPA is providing the class and what you need is implementing the interface which uses the implementation comes with Spring Data JPA
- * Spring Data JPA comes with one common repository called "CrudRepository<>". This is an interface and you just have to extend this. 
- * Your custom repository is extended by "CrudRepository<>" which contains all the common methods/CRUD operations.
+ * Spring Data JPA comes with one common repositories called "CrudRepository<>". This is an interface and you just have to extend this.
+ * Your custom repositories is extended by "CrudRepository<>" which contains all the common methods/CRUD operations.
  * CrudRepository is a generic type.So you need to provide that generic type information. Here you need to provide two types of generic information
  * First one is what is the entity class is here working with(Here "Part"). The second generic type is what is the Id that entity class has(Here it is a "String").
  */
-public interface PartRepository extends CrudRepository<Part, String> {
+public interface PartRepository extends JpaRepository<Part, String> {
 	
 	/**
 	 * Should start the method signature from the word "find" and complete the rest exactly from the filter criteria.
--- src/main/java/it/nmadlk/repositories/VehicleRepository.java
+++ src/main/java/it/nmadlk/repositories/VehicleRepository.java
@@ -1,9 +1,9 @@
 /**
  * 
  */
-package it.nmadlk.repository;
+package it.nmadlk.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import it.nmadlk.models.Vehicle;
 
@@ -11,6 +11,6 @@ import it.nmadlk.models.Vehicle;
  * @author Navo
  *
  */
-public interface VehicleRepository extends CrudRepository<Vehicle, String> {
+public interface VehicleRepository extends JpaRepository<Vehicle, String> {
 
 }
