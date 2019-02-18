e4b4ba70cf7decd5dd7cd7e319927a5c5464cf93
nmadlk <nirmalauwucst@gmail.com>
2019-02-18 08:18:20 +0530

    Added some ES repository methods

--- src/main/java/it/nmadlk/searchRepositories/PartESRepository.java
+++ src/main/java/it/nmadlk/searchRepositories/PartESRepository.java
@@ -3,10 +3,14 @@ package it.nmadlk.searchRepositories;
 import it.nmadlk.models.Part;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.List;
+
 
 /**
  * @author Navo
  *
  */
 public interface PartESRepository extends ElasticsearchRepository<Part, String> {
+
+    List<Part> findByVehicle_Make(String vehicleMake);
 }
--- src/main/java/it/nmadlk/searchRepositories/VehicleESRepository.java
+++ src/main/java/it/nmadlk/searchRepositories/VehicleESRepository.java
@@ -3,9 +3,13 @@ package it.nmadlk.searchRepositories;
 import it.nmadlk.models.Vehicle;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.List;
+
 /**
  * @author Navo
  *
  */
 public interface VehicleESRepository extends ElasticsearchRepository<Vehicle, String> {
+
+    List<Vehicle> findByMake(String make);
 }
